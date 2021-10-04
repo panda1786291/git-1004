0b7898659f2b991da9ec3c36ebbde71033ec59e2
zjk <zjk@qq.com>
2021-10-04 17:40:55 +0800

    hotfix02

--- src/main/java/com/zjk/git.java
+++ src/main/java/com/zjk/git.java
@@ -5,5 +5,6 @@ public class git {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello hot fix 02");
     }
 }
